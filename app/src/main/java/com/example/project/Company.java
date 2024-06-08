b89d838ad01880c151f2464c971244b2bf2f5fb1
malak khalaf <malakKhalaf2001@gmail.com>
2024-06-08 10:32:05 +0300

    malak

---
+++ app/src/main/java/com/example/project/Company.java
@@ -0,0 +1,97 @@
+package com.example.project;
+
+
+public class Company {
+    private String name;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String image;
+    private String phone;
+    private String facebookLink;
+
+    // Constructor
+    public Company(String name, String email, String password, String confirmPassword, String image, String phone, String facebookLink) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.image = image;
+        this.phone = phone;
+        this.facebookLink = facebookLink;
+    }
+    public Company(String name, String email) {
+        this.name = name;
+        this.email = email;
+
+    }
+    // Getters and Setters
+    public  String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public  String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFacebookLink() {
+        return facebookLink;
+    }
+
+    public void setFacebookLink(String facebookLink) {
+        this.facebookLink = facebookLink;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                ", image='" + image + '\'' +
+                ", phone='" + phone + '\'' +
+                ", facebookLink='" + facebookLink + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/project/ImagesAdapter.java
@@ -0,0 +1,64 @@
+package com.example.project;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class ImagesAdapter extends RecyclerView.Adapter<ImagesAdapter.ViewHolder> {
+    private Context context;
+    private List<Company> items;
+
+    public ImagesAdapter(Context context, List<Company> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_images,
+                parent,
+                false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final Company company = items.get(position);
+        CardView cardView = holder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.image);
+        Glide.with(context).load(company.getImage()).into(imageView);
+        TextView txt = cardView.findViewById(R.id.txtName);
+        txt.setText(company.getName());
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView;
+
+        public ViewHolder(CardView cardView) {
+            super(cardView);
+            this.cardView = cardView;
+        }
+    }
+}
